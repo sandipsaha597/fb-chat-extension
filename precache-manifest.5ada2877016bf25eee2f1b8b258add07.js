46c01ac892de58c4373b9835074e53b37fd40471
sandipsaha597 <40121135+sandipsaha597@users.noreply.github.com>
2019-05-24 07:05:26 +0530

    Add files via upload

---
+++ precache-manifest.5ada2877016bf25eee2f1b8b258add07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c713f94897d8bbcf0065f1010c9077",
+    "url": "/fb-chat-extension/index.html"
+  },
+  {
+    "revision": "f92a1d640cb690d41b98",
+    "url": "/fb-chat-extension/static/css/main.2557d500.chunk.css"
+  },
+  {
+    "revision": "62a452cd739ba532efbc",
+    "url": "/fb-chat-extension/static/js/2.0f2f8d79.chunk.js"
+  },
+  {
+    "revision": "f92a1d640cb690d41b98",
+    "url": "/fb-chat-extension/static/js/main.c1f2ae03.chunk.js"
+  },
+  {
+    "revision": "2d4a2014faf8e373c9de",
+    "url": "/fb-chat-extension/static/js/runtime~main.c394d1ae.js"
+  }
+]);
